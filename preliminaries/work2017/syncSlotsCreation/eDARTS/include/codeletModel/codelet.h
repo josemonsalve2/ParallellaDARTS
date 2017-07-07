6a159d49a80bc425b1e834c222ab65e0a3a65f5d
Jose M Monsalve Diaz <josem@udel.edu>
2017-07-07 20:20:41 +0000

    Creating codelets

---
+++ preliminaries/work2017/syncSlotsCreation/eDARTS/include/codeletModel/codelet.h
@@ -0,0 +1,12 @@
+/**
+ * @file codelet.h
+ * @author Jose M Monsalve
+ * @date 26 May 2017
+ * @brief Definition of the synchronization slots
+ * @todo Add copyright
+ *
+ * Syncronization slots are the part of the runtime that 
+ * receives the signals from previous events or codelets. They 
+ * express when the required data, control or resource
+ * dependecies have been already satisfied. 
+ */
