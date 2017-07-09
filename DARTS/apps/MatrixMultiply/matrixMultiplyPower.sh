415c865ea7049dfdce4d22b8b9afab3af47c0bd2
Jose Monsalve Diaz <josem@udel.edu>
2017-07-09 03:30:09 +0000

    Deleting original DARTS directory

--- DARTS/apps/MatrixMultiply/matrixMultiplyPower.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#put the best tile
-tile=32;
-
-./mmPower 2 15 2 1 33554432 $tile $tile >> mmPower.txt
-./mmPower 2 15 3 2 33554432 $tile $tile >> mmPower.txt
-./mmPower 2 15 3 3 33554432 $tile $tile >> mmPower.txt
--- DARTS/apps/Mergesort/mergesortPower.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#put the best base
-base=2048;
-
-./mergesortPower 2 15 2 1 33554432 $base >> mergePower.txt
-./mergesortPower 2 15 3 2 33554432 $base >> mergePower.txt
-./mergesortPower 2 15 3 3 33554432 $base >> mergePower.txt
