36624ddaa3a665368a9704e5e1b7143cbc466757
Jose Monsalve Diaz <josem@udel.edu>
2017-07-24 20:04:50 +0000

    Cleaning code. Starting with the multiple to multiple

--- eDARTS/test/assets/tpClosuresQueue/e_tpClosuresQueue_test.c
+++ eDARTS/test/assets/tpClosuresQueue/e_tpClosuresQueue_test.c
@@ -234,9 +234,7 @@ void e_multipleProducersSingleConsumer() {
         } else if (e_group_config.core_row == 0 && e_group_config.core_col == 1){
             simple_tp_tpClosure_t newSimpleTpClosure = _invoke_simple_tp();
             unsigned code;
-            while ((code = pushTpClosureQueue(myTpQueue,(genericTpClosure_t*)(&newSimpleTpClosure))) != TPC_QUEUE_SUCCESS_OP){
-                //e_darts_print("Core (%d,%d) failed to push with code = %d \n",e_group_config.core_row, e_group_config.core_col,code);
-            }
+            while ((code = pushTpClosureQueue(myTpQueue,(genericTpClosure_t*)(&newSimpleTpClosure))) != TPC_QUEUE_SUCCESS_OP){}
 
             // Create the tpClosure and pushing it
             complex_tp_tpClosure_t newComplexTpClosure = _invoke_complex_tp(5,6);
@@ -246,17 +244,100 @@ void e_multipleProducersSingleConsumer() {
         } else {
             simple_tp_tpClosure_t newSimpleTpClosure = _invoke_simple_tp();
             unsigned code;
-            while ((code = pushTpClosureQueue(myTpQueue,(genericTpClosure_t*)(&newSimpleTpClosure))) != TPC_QUEUE_SUCCESS_OP){
-                //e_darts_print("Core (%d,%d) failed to push with code = %d \n",e_group_config.core_row, e_group_config.core_col,code);
-            }
+            while ((code = pushTpClosureQueue(myTpQueue,(genericTpClosure_t*)(&newSimpleTpClosure))) != TPC_QUEUE_SUCCESS_OP){}
             darts_barrier(myBarrier);
-
         }
-
 }
 
 void e_multipleProducersMultipleConsumers() {
+    if (e_group_config.core_row == 0 && e_group_config.core_col == 0) {
+        e_darts_print("\n--- MULTIPLE PRODUCERS SINGLE CONSUMER TEST ---\n");
+        //Here we have the consumers
+        unsigned base0_0Address = (unsigned) e_get_global_address( 0 , 0 , 0x0000 );
 
+        // while the final tp has not been submitted
+        // Since we only have one single consumer we can own the queue at the beginning
+        unsigned numCreatedTps = 0, i, j, k, stillExecuting = 0;
+        ownTpClosureQueue(myTpQueue);
+        while (*finalSignal != 1 || stillExecuting == 1) { // Final signal is set by a TP
+            stillExecuting = 0;
+            if (!isTpClosureQueueEmpty(myTpQueue)) {
+                genericTpClosure_t * currentTpClosure = 0;
+                // SU scheduling
+                // Check if we successfully pop a tpClosure
+                if (peakTopElement(myTpQueue,&currentTpClosure) == TPC_QUEUE_SUCCESS_OP) {
+                    // TP allocation
+
+                    _tp_metadata_t * actualTP = (_tp_metadata_t *) myTpHeap;
+                    *actualTP = currentTpClosure->_metadataCtor(numCreatedTps);
+                    numCreatedTps++;
+                    // Move the heap pointer
+                    myTpHeap = (void *) (((unsigned)myTpHeap) + sizeof(_tp_metadata_t) + (sizeof(syncSlot_t)*actualTP->numSyncSlots));
+                    // Memory allocation for Local
+                    if (actualTP->sizeLocal != 0) {
+                        actualTP->memLocal = myLocalMemHeap;
+                        myLocalMemHeap = (void *) (base0_0Address + (unsigned)myLocalMemHeap + actualTP->sizeLocal);
+                    }
+                    // Memory allocation for DRAM
+                    if (actualTP->sizeDRAM != 0) {
+                        actualTP->memDRAM = myDRAMMemHeap;
+                        myDRAMMemHeap = (void *) ((unsigned)myDRAMMemHeap + actualTP->sizeDRAM);
+                    }
+
+                    // Now we can call the user Constructor
+                    currentTpClosure->_userInitCtorExec(actualTP,currentTpClosure);
+
+                    // Last thing is to delete that already processed element from the queue
+                    popTopElementQueue(myTpQueue);
+                }
+            }
+            // Codelet Scheduling
+            // Iterate over the TPs to find codelets ready to execute
+            *currentExecTpAddr = (unsigned) (TP_HEAP_LOCATION);
+            _tp_metadata_t * currentTp = (_tp_metadata_t *) (*currentExecTpAddr);
+            for (i = 0; i < numCreatedTps; i ++) {
+                syncSlot_t * currentSyncSlot = (syncSlot_t *) ( (*currentExecTpAddr) + sizeof(_tp_metadata_t));
+                // Iterate over the syncSlots to find codelets that are ready to be executed
+                for (j = 0; j<currentTp->numSyncSlots; j++) {
+                    if (currentSyncSlot->currentDep == 0) {
+                        // Ready to execute
+                        codelet_t codeletToExec = currentSyncSlot->codeletTemplate;
+                        for (k = 0; k < currentSyncSlot->numCodelets; k++) {
+                            codeletToExec.fire();
+
+                        }
+                        stillExecuting = 1; // TODO This might not work
+                        syncSlotResetDep(currentSyncSlot);
+                    }
+                    // next SyncSlot
+                    currentSyncSlot++;
+                }
+                *currentExecTpAddr = ((*currentExecTpAddr) + sizeof(_tp_metadata_t) + (sizeof(syncSlot_t)*currentTp->numSyncSlots));
+                currentTp = (_tp_metadata_t *) (*currentExecTpAddr);
+            }
+        }
+
+        // We are done with the queue, we disown it
+        disownTpClosureQueue(myTpQueue);
+
+        darts_barrier(myBarrier);
+    } else if (e_group_config.core_row == 0 && e_group_config.core_col == 1){
+        simple_tp_tpClosure_t newSimpleTpClosure = _invoke_simple_tp();
+        unsigned code;
+        while ((code = pushTpClosureQueue(myTpQueue,(genericTpClosure_t*)(&newSimpleTpClosure))) != TPC_QUEUE_SUCCESS_OP){}
+
+        // Create the tpClosure and pushing it
+        complex_tp_tpClosure_t newComplexTpClosure = _invoke_complex_tp(5,6);
+        while (pushTpClosureQueue(myTpQueue,(genericTpClosure_t*)(&newComplexTpClosure)) != TPC_QUEUE_SUCCESS_OP);
+
+        darts_barrier(myBarrier);
+    } else {
+        simple_tp_tpClosure_t newSimpleTpClosure = _invoke_simple_tp();
+        unsigned code;
+        while ((code = pushTpClosureQueue(myTpQueue,(genericTpClosure_t*)(&newSimpleTpClosure))) != TPC_QUEUE_SUCCESS_OP){}
+        darts_barrier(myBarrier);
+
+    }
 }
 
 void e_checkingPaddingAndErrors() {
