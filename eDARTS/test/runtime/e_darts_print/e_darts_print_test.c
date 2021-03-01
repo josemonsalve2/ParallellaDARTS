aa25333b3c59bae25d1ea88be37a8d119f5953ea
Dawson Fox <dawsfox@udel.edu>
2021-03-01 18:42:18 -0500

    added better float testing to print test

--- eDARTS/test/runtime/e_darts_print/e_darts_print_test.c
+++ eDARTS/test/runtime/e_darts_print/e_darts_print_test.c
@@ -5,12 +5,25 @@
 
 #define FINAL_BARRIER 0x2008
 
+void vectorTesting();
+
 int main(void)
 {
     unsigned * finalBarrier = (unsigned *) FINAL_BARRIER;
     e_darts_print("Hello world ... %d %f %d \n",e_group_config.core_row, 333.33,e_group_config.core_col);
+    vectorTesting();
     int i = 0 ;
     while (i++ < 1000);
     *finalBarrier = 1;
     return 0;
+}
+
+void vectorTesting()
+{
+    float vector[8];
+    for (int i=0; i<8; i++) {
+        vector[i] = 1.0;
+    }
+    e_darts_print("[ %f %f %f %f %f %f %f %f ]\n", vector[0], vector[1], vector[2], vector[3], vector[4], vector[5], vector[6], vector[7]);
+    e_darts_print("vector addresses: [ %x %x %x %x %x %x %x %x ]\n", &(vector[0]), &(vector[1]), &(vector[2]), &(vector[3]), &(vector[4]), &(vector[5]), &(vector[6]), &(vector[7]));
 }
